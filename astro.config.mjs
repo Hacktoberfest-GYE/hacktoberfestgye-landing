83fdd33082c4950de922cbc0080990b50d54363f
Brayen Gavilanes <bjgavilanes@gmail.com>
2024-10-12 07:52:24 +0000

    github-pages: put custom pages in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://hacktoberfestgye.org',
   trailingSlash: 'never',
   build: {
     format: 'file',
